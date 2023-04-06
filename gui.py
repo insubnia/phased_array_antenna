44a4ebd70b2a43a75600c768d3197132ed75de24
Insub Song <sis@skaichips.co.kr>
2023-04-06 21:54:18 +0900

    update remap table

--- gui.py
+++ gui.py
@@ -27,10 +27,10 @@ def resource_path(relative_path):
 
 def remap(x):
     table = (
-        0, 1, 2, 3,
-        4, 5, 6, 7,
-        8, 9, 10, 11,
-        12, 13, 14, 15,
+        15, 14, 13, 12,
+        11, 10, 9, 8,
+        7, 6, 5, 4,
+        3, 2, 1, 0,
     )
     return table[x]
 
